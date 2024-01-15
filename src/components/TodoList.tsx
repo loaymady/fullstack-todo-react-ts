2f3dd70923f664e2c52131b4144d41118f63b851
Loay Mady <loay.mady13@gmail.com>
2024-01-15 18:22:06 +0200

    UseEffect to fetch Data(before Delete)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,37 +1,40 @@
+import { useEffect, useState } from "react";
 import Button from "./ui/Button";
+import axiosInstance from "../config/axios.config";
+
+const storageKey = "loggedInUser";
+const userDataString = localStorage.getItem(storageKey);
+const userData = userDataString ? JSON.parse(userDataString) : null;
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axiosInstance
+      .get("/users/me?populate=todos", {
+        headers: {
+          Authorization: `Bearer ${userData.jwt}`,
+        },
+      })
+      .then((res) => {
+        console.log(setTodos(res.data.todos));
+      });
+  }, []);
   return (
     <div className="space-y-1 ">
-      <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-        <p className="w-full font-semibold">1 - First Todo</p>
-        <div className="flex items-center justify-end w-full space-x-3">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Remove
-          </Button>
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100"
+        >
+          <p className="w-full font-semibold">{todo.title}</p>
+          <div className="flex items-center justify-end w-full space-x-3">
+            <Button size={"sm"}>Edit</Button>
+            <Button variant={"danger"} size={"sm"}>
+              Remove
+            </Button>
+          </div>
         </div>
-      </div>
-
-      <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-        <p className="w-full font-semibold">2 - Second Todo</p>
-        <div className="flex items-center justify-end w-full space-x-3">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Remove
-          </Button>
-        </div>
-      </div>
-
-      <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-        <p className="w-full font-semibold">3 - Third Todo</p>
-        <div className="flex items-center justify-end w-full space-x-3">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Remove
-          </Button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
