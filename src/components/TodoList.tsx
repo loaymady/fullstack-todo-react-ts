8aab4d4b57ecc738fb7c9e8cadae830b019c3102
Loay Mady <loay.mady13@gmail.com>
2024-01-17 22:43:26 +0200

    React Query

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,34 +1,32 @@
-import { useEffect, useState } from "react";
 import Button from "./ui/Button";
 import axiosInstance from "../config/axios.config";
+import { useQuery } from "@tanstack/react-query";
 
 const storageKey = "loggedInUser";
 const userDataString = localStorage.getItem(storageKey);
 const userData = userDataString ? JSON.parse(userDataString) : null;
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axiosInstance
-      .get("/users/me?populate=todos", {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get("/users/me?populate=todos", {
         headers: {
           Authorization: `Bearer ${userData.jwt}`,
         },
-      })
-      .then((res) => {
-        setTodos(res.data.todos);
-      })
-      .finally(() => {
-        setLoading(false);
       });
-  }, []);
-  if (loading) return <h3>Loading...</h3>;
+      return data.todos;
+    },
+  });
+  console.log({ isPending, error, data });
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <div className="space-y-1 ">
-      {todos.length ? (
-        todos.map((todo: { id: number; title: string }) => (
+      {data.length ? (
+        data.map((todo: { id: number; title: string }) => (
           <div
             key={todo.id}
             className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100"
